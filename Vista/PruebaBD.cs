10d79f67fc8a034d076ba52d8c9451954a8568b5
SilvanaBgn <silvana.belen.garcia@gmail.com>
2017-05-31 23:01:17 -0300

    PruebaBD

---
+++ Vista/PruebaBD.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CONTROLADOR;
+using ENTIDAD;
+
+namespace Vista
+{
+    public partial class PruebaBD : Form
+    {
+        ControladorArchivo iControladorArchivo;
+        ControladorBuscar iControladorBuscar;
+        ControladorEntretenimiento iControladorEntretenimiento;
+        ControladorRedSocial iControladorRedSocial;
+        ControladorUsuario iControladorUsuario;
+
+        public PruebaBD()
+        {
+            InitializeComponent();
+            this.iControladorArchivo = new ControladorArchivo();
+            this.iControladorBuscar = new ControladorBuscar();
+            this.iControladorEntretenimiento = new ControladorEntretenimiento();
+            this.iControladorRedSocial = new ControladorRedSocial();
+            this.iControladorUsuario = new ControladorUsuario();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //RedSocial mRedSocial = new RedSocial("https://www.facebook.com/",);
+            //this.iControladorRedSocial
+        }
+    }
+}
