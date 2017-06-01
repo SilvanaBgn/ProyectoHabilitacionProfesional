04aedcb7db20a49cb9f0357b573887cc1f757073
SoyOtroJulianAcosta <julian_acosta77@hotmail.com>
2017-05-31 22:37:22 -0300

    ItemLateral y Principal - Primer commit.

---
+++ Vista/ItemLateral.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vista
+{
+    public partial class ItemLateral : UserControl
+    {
+        //nombre formal de la pantalla
+        private String nombrePantalla;
+
+        //nombre del form a levantar. 
+        private String nombreForm;
+
+        public ItemLateral()
+        {
+            InitializeComponent();
+        }
+    }
+}
